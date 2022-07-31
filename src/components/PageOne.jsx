373702636014cfe419dd9cc2f4dedf7e417d36ce
Keelan <keelanmatthews123@gmail.com>
2022-07-31 09:22:06 +0200

    Implemented react bootstrap

--- src/components/PageOne.jsx
+++ src/components/PageOne.jsx
@@ -1,20 +1,37 @@
-import React, { useState} from 'react'
+import React, { useState } from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function PageOne() {
 
     return (
-        <div className='page-1'>
-            <div style={{ width: '1700px' }} id="panel-1">
+        <Container fluid>
+            <Row id="panel-1">
                 <img src="page1/panel-1.png" alt="" style={{ maxWidth: '100%' }} />
-            </div>
-            <div className='d-flex'>
-                <div style={{ width: '1200px' }} id="panel-2">
+            </Row>
+            <Row className='d-flex'>
+                <Col lg="7" id="panel-2">
                     <img src="page1/panel-2.png" alt="" style={{ maxWidth: '100%' }} />
-                </div>
-                <div style={{ width: '400px' }} id="panel-3">
+                </Col>
+                <Col lg="5" id="panel-3">
                     <img src="page1/panel-3.png" alt="" style={{ maxWidth: '100%' }} />
-                </div>
-            </div>
-        </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col lg="4">
+                <img src="page1/panel-4.png" alt="" style={{ maxWidth: '100%' }} />
+                </Col>
+                
+                <Col>
+                    <Row>
+                        <img src="page1/panel-5.png" alt="" style={{ maxWidth: '100%' }} />
+                    </Row>
+                    <Row>
+                        <img src="page1/panel-6.png" alt="" style={{ maxWidth: '100%' }} />
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
     )
 }
