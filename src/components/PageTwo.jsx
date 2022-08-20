38dbdf6a9301ca9db099ca0c7703d3e29678ff4d
Keelan <keelanmatthews123@gmail.com>
2022-08-20 13:36:15 +0200

    finished page 2 animations

--- src/components/PageTwo.jsx
+++ src/components/PageTwo.jsx
@@ -9,7 +9,7 @@ import Appear from './animations/Appear';
 export default function PageTwo() {
 
     return (
-        <Container fluid className="my-5 py-5">
+        <Container fluid className="mt-5 py-5">
             <Row className="my-2">
                 <Col className='p2p1' lg={4}>
                     <SlideLeft img="page2/panel-1.riv" isRive="true" />
@@ -17,24 +17,24 @@ export default function PageTwo() {
                 <Col className='p2p2' lg={4}>
                     <Appear img="page2/panel-2.riv" isRive="true" delayNum="1" />
                 </Col>
-                <Col lg={4}>
-                    <SlideRight img="page2/panel-3.jpg" delayNum="2" />
+                <Col className='p2p1' lg={4}>
+                    <SlideRight img="page2/panel-3.riv" isRive="true" delayNum="2" />
                 </Col>
             </Row>
             <Row className="my-2">
                 <Col>
-                    <SlideLeft img="page2/panel-4.jpg" delayNum="2.2" />
+                    <SlideLeft img="page2/panel-4.jpg" delayNum="1" />
                 </Col>
                 <Col>
-                    <SlideRight img="page2/panel-5.jpg" delayNum="3" />
+                    <SlideRight img="page2/panel-5.jpg" delayNum="1.5" />
                 </Col>
             </Row>
-            <Row className="my-2">
+            <Row className="my-4">
                 <Col lg={7}>
                     <SlideLeft img="page2/panel-6.jpg" />
                 </Col>
-                <Col lg={5}>
-                    <SlideRight img="page2/panel-7.jpg" delayNum="1" />
+                <Col lg={5} className="p2p7">
+                    <SlideRight img="page2/panel-7.riv" isRive="true" delayNum="1" />
                 </Col>
             </Row>
             <Row className="my-2 p2p8">
