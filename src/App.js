1455aa0abcf7b4a4dbafed6bb31868178b34b3cc
Keelan <keelanmatthews123@gmail.com>
2022-08-20 18:32:58 +0200

    finished page 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import PageOne from './components/PageOne'
 import PageTwo from './components/PageTwo'
+import PageThree from './components/PageThree';
 import { useInView } from "react-intersection-observer"
 
 function App() {
-	const [night, setNight] = useState(false)
+	const [light, setNight] = useState(false)
 
 	const [ref, inView] = useInView({
         threshold: 0.3,
@@ -16,13 +17,23 @@ function App() {
     }, [inView]);
 
 	return (
-		<main className={`${night ? "bg-dark" : ""}`}>
+		<main className={`${light ? "" : "bg-dark"}`}>
 			<div className="container">
-				<PageOne />
-				<div className='page-gap'></div>
-				
 				<div ref={ref}>
-					<PageTwo />
+					<PageOne />
+				</div>
+				
+				<div className='page-gap d-flex justify-content-center align-items-center'>
+					<h1 className='display-1 fw-bold'>Later that evening...</h1>
+				</div>
+				
+				<PageTwo />
+				<PageThree/>
+
+				<div className='page-gap'/>
+
+				<div className='page-gap d-flex justify-content-center align-items-center'>
+					<h1 className='display-3 fw-bold text-white'>The night of the presentation...</h1>
 				</div>
 			</div>
 		</main>
---
+++ src/components/PageThree.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import SlideRight from './animations/SlideRight';
+import SlideLeft from './animations/SlideLeft';
+import Appear from './animations/Appear';
+import useSound from 'use-sound';
+
+export default function PageThree() {
+    const [play, { stop }] = useSound("audio/page3/panel-4.mp3");
+
+    return (
+        <Container fluid className="mt-5 py-5">
+            <Row className="my-2">
+                <Col lg={6}>
+                    <SlideLeft img="page3/panel-1.jpg" />
+                </Col>
+                <Col className="p3p2" lg={6}>
+                    <SlideRight img="page3/panel-2.riv" isRive="true" delayNum="1" sound="audio/page3/panel-2.mp3" />
+                </Col>
+            </Row>
+            <Row className="my-4">
+                <Col>
+                    <SlideLeft img="page3/panel-3.jpg" />
+                </Col>
+            </Row>
+            <Row className="my-2" onMouseEnter={() => play()} onMouseLeave={() => stop()}>
+                <Col className='p3p4'>
+                    <Appear img="page3/panel-4.riv" isRive="true" />
+                </Col>
+            </Row>
+        </Container>
+    )
+}
