827e870cc5d12a8595f76ccd17beb434d25cd1ea
Keelan <keelanmatthews123@gmail.com>
2022-10-28 21:17:13 +0200

    implemented scroll down

--- src/App.js
+++ src/App.js
@@ -7,14 +7,20 @@ import { useInView } from "react-intersection-observer"
 
 function App() {
 	const [light, setNight] = useState(false)
+	const [scroll, setScroll] = useState(false)
 
 	const [ref, inView] = useInView({
 		threshold: 0.3,
 		rootMargin: '500px 0px 0px 0px'
 	});
 
+	const scrollInterval = setInterval(() => {
+		setScroll(window.scrollY > 0)
+	}, 500)
+
 	useEffect(() => {
 		setNight(inView)
+		clearInterval(scrollInterval)
 	}, [inView]);
 
 	return (
@@ -23,8 +29,13 @@ function App() {
 				<div className="m-5 p-2 px-3 border-black d-inline-block">
 					<a href="https://keelanmatthews.com/projects" className='text-decoration-none text-dark'>Go back</a>
 				</div>
-				<p className='text-dark fs-2'>02</p>
+				<p className='text-dark fs-1 me-5'>02</p>
 			</div>
+			<div className='position-absolute d-flex flex-column justify-content-center align-items-center' style={{ right: 0, bottom: 0 }}>
+				<p className={`fs-5 scroll-down-text ${scroll ? 'hide' : ''}`}>scroll down</p>
+				<div className={`mt-5 scroll-line ${scroll ? 'hide' : ''}`}></div>
+			</div>
+
 
 			<div className="container">
 				<div ref={ref}>
